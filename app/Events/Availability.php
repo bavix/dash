3906f2989bad6c5f1eb7c11ffe43535d257e5b67
Babichev Maxim <babichev@mail.tvil.ru>
2019-01-25 18:55:33 +0300

    add event

---
+++ app/Events/Availability.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class Availability implements ShouldBroadcast
+{
+
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var string
+     */
+    protected $service;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param string $service
+     * @param string $state
+     */
+    public function __construct(string $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('availability');
+    }
+
+}
