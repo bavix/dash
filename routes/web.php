e89e9539ab2222da56e2dcd9c55005ef0d12be70
Babichev Maxim <babichev@mail.tvil.ru>
2019-02-01 18:28:21 +0300

    Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::view('/', 'dashboard');
