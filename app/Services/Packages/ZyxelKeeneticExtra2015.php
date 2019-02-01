fc620444f3b7ddac420716c21ed4cd9fb93dbd59
Babichev Maxim <babichev@mail.tvil.ru>
2019-02-01 18:31:52 +0300

    add zyxel

---
+++ app/Services/Packages/ZyxelKeeneticExtra2015.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services\Packages;
+
+use App\Services\Router;
+
+class ZyxelKeeneticExtra2015 extends Router
+{
+
+    /**
+     * @var string
+     */
+    protected $title = 'Keenetic';
+
+    /**
+     * @var string
+     */
+    protected $color = 'is-info';
+
+}
