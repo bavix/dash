d9f1631a4442f103263d5ac4d8ac2eea180ad860
Babichev Maxim <info@babichev.net>
2019-02-08 23:04:05 +0300

    add guzzle

--- app/Services/Router.php
+++ app/Services/Router.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Client;
+
 abstract class Router extends Package
 {
 
@@ -31,28 +33,34 @@ abstract class Router extends Package
     protected $stopAllowed = false;
 
     /**
+     * @var Client
+     */
+    protected $guzzle;
+
+    /**
+     * Router constructor.
+     * @param array $options
+     */
+    public function __construct(array $options)
+    {
+        parent::__construct($options);
+        $this->guzzle = new Client([
+            'base_uri' => $this->url,
+            'timeout'  => 10,
+        ]);
+    }
+
+    /**
      * @return bool
      */
     public function active(): bool
     {
-        $ch = \curl_init();
-        \curl_setopt($ch, CURLOPT_URL, $this->url);
-        \curl_setopt($ch, CURLOPT_HEADER, true);
-        \curl_setopt($ch, CURLOPT_NOBODY, true);
-        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        \curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        \curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-        \curl_exec($ch);
-
-        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        \curl_close($ch);
-
-        if ($code === 0) {
-            $header = \get_headers($this->url, 1)[0] ?? 'HTTP/1.1 400 BAD REQUEST';
-            $code = (int)(\explode(' ', $header, 3)[1] ?? 400);
-        }
-
-        $this->active = \in_array($code, [200, 201, 401], true);
+        $response = $this->guzzle->get('/');
+        $this->active = \in_array(
+            $response->getStatusCode(),
+            [200, 201, 401],
+            true
+        );
 
         return parent::active();
     }
