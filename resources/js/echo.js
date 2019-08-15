19d5ad16328fb1f54aa36e8ac22380f39fa930b9
Babichev Maxim <maksim.babichev95@gmail.com>
2019-08-15 22:59:42 +0300

    add transports (cors)

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,5 +3,10 @@ import Echo from 'laravel-echo'
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001'
+    host: window.location.hostname + ':6001',
+
+    /**
+     * @see https://github.com/tlaverdure/laravel-echo-server/issues/162#issuecomment-415830727
+     */
+    transports: ['websocket', 'polling', 'flashsocket'],
 });
