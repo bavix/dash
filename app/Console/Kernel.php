9c81db1e9eeabdcec25585c99cc8e6c1253dbed3
Babichev Maxim <info@babichev.net>
2019-12-13 19:55:26 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // 
+        $schedule->command('service:check')
+            ->everyMinute();
     }
 
     /**
